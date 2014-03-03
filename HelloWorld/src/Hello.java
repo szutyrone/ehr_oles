211aa1e2f7578b4b5462e587915d3bb54dca6282
ben <107622067@qq.com>
2014-03-03 17:57:39 +0800

    Only need say hello 1.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,8 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Hell2");
-		System.out.println("Hello2");
 	}
 
 }
