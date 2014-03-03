98478ac43048928ce143861c7f1b7acb5f18cb7e
szutyrone <szutyrone@users.noreply.github.com>
2014-03-03 15:24:36 +0800

    Update Hello.java
    Add the third message.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,10 +1,10 @@
-
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("Hell2");
+		System.out.println("Hello2");
 	}
 
 }
